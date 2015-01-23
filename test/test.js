27c61d4b1afbc1b0dbe6a0a983b8a743bc408b05
anthony <anthony95356@gmail.com>
2015-01-24 00:27:12 +0100

    Add run file test.js for mocha test, and add code coverage  made by Blanket js
    Mocha is a feature-rich JavaScript test framework running on node.js and the browser,
    making asynchronous testing simple and fun.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,57 @@
 require("must");
-
-var obj = "an";
-var mcf = require('mcf');
-var user ="anthony";
-
+var mcf = require('../');
+var assert = require("assert");
 var mcf_field = "$pbkdf2$10000$salt$derived";
+var mcf_cost_null= "$pbkdf2$0$salt$derived";
+var mcf_cost_negative = "$pbkdf2$-10$salt$derived";
 
-var obj = mcf.deserialize(mcf_field);
-obj.must.be.an.object();
 
-var tabString = mcf.serialize("pbkdf2,10000,salt,derived");
-tabString.must.be.a.string();
+
+//var mcf_index = require('index.js');  // our module
+
+describe('mcf', function(){
+  describe('Module mcf_index', function(){
+    it('should have a serialize adn deserialize Method', function(){
+      assert.equal(typeof mcf, 'object');
+      assert.equal(typeof mcf.deserialize, 'function');
+      assert.equal(typeof mcf.serialize, 'function');
+    });
+  });
+
+
+  describe('#serialize', function(){
+    it('serialize("pbkdf2,10000,salt,derived"should equal : $pbkdf2$10000$salt$derived ',function(){
+      var testSerialize = mcf.serialize("pbkdf2",10000,"salt","derived");
+      testSerialize.must.equal("$pbkdf2$10000$salt$derived");
+      testSerialize.must.be.a.string();
+    });
+  });
+  
+  describe("#deserialize",function(){
+    it('deserialize(mcf_field) should return an object [pbkdf2,10000,salt,derived]',function(){
+    
+      //assert.deepEqual(mcf.deserialize(mcf_field),['identifier:pbkdf2','cost:10000','salt:salt','derived_key:derived']);
+      var testDeserialize = mcf.deserialize(mcf_field);
+      testDeserialize.must.be.an.object();
+    
+    });
+  });
+
+
+  describe("#serialize cost is null",function(){
+    it("deserialize(mcf_cost_null) should return an error", function(){
+     
+      //mcf.deserialize(mcf_cost_null).bind(mcf).must.throw();
+      
+    });
+
+  });
+
+
+
+
+
+});
+
 
 
