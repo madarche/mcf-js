b9ef827d72991d3528ad3d9fa8bc5477e3d8bee3
anthony <anthony95356@gmail.com>
2015-01-28 15:00:43 +0100

    Update test.js spaces have been removed

--- test/test.js
+++ test/test.js
@@ -16,37 +16,29 @@ describe('mcf', function(){
 
   describe('#serialize', function(){
     it("serialize(pbkdf2,10000,salt,derived) should equal : $pbkdf2$10000$salt$derived and must be a string",function() {
-
       var testSerialize = mcf.serialize("pbkdf2",10000,"salt","derived");
       testSerialize.must.equal("$pbkdf2$10000$salt$derived");
       testSerialize.must.be.a.string();
-
     });
   });
   
   describe("#deserialize",function(){
     it("deserialize(mcf_field) should return an object [pbkdf2,10000,salt,derived]",function() {
-
       var testDeserialize = mcf.deserialize(mcf_field);
       testDeserialize.must.include("pbkdf2",10000,"salt","derived");
       testDeserialize.must.be.an.object();
-
     });
   });
 
   describe("#serialize cost is null",function(){
     it("deserialize(mcf_cost_null) should return an error", function() {
-
       mcf.deserialize.bind(mcf_cost_null).must.throw();
-      
     });
   });
 
   describe("#serialize error regex",function(){
     it("deserialize(mcf_cost_error) should return an error of regex", function() {
-
-       mcf.deserialize.bind(mcf_cost_error).must.throw();
-
+      mcf.deserialize.bind(mcf_cost_error).must.throw();
     });
   });
 
