e2130f973957d0d7871f150fb1addaaee78e9385
Marc-Aurèle DARCHE <ma.darche@cynode.org>
2015-04-07 22:37:25 +0200

    Improve tests

--- test/test.js
+++ test/test.js
@@ -1,45 +1,34 @@
 require('must');
 var mcf = require('../');
-var assert = require('assert');
-var mcf_field = '$pbkdf2$10000$salt$derived';
-var mcf_cost_null = '$pbkdf2$0$salt$derived';
-var mcf_cost_error = '$-15pbkdf2$0$salt$derived';
 
-describe('mcf', function() {
-    describe('Module mcf_index', function() {
-        it("should have a serialize adn deserialize Method", function() {
-            assert.equal(typeof mcf, 'object');
-            assert.equal(typeof mcf.deserialize, 'function');
-            assert.equal(typeof mcf.serialize, 'function');
-        });
+describe("MCF", function() {
+
+    it("serialize(pbkdf2,10000,salt,derived) should equal: $pbkdf2$10000$salt$derived and must be a string", function() {
+        var testSerialize = mcf.serialize("pbkdf2", 10000, "salt", "derived");
+        testSerialize.must.equal("$pbkdf2$10000$salt$derived");
+        testSerialize.must.be.a.string();
     });
 
-    describe('#serialize', function() {
-        it("serialize(pbkdf2,10000,salt,derived) should equal : $pbkdf2$10000$salt$derived and must be a string",function() {
-            var testSerialize = mcf.serialize("pbkdf2",10000,"salt","derived");
-            testSerialize.must.equal("$pbkdf2$10000$salt$derived");
-            testSerialize.must.be.a.string();
-        });
+    it("deserialize(mcf_field) should return an object [pbkdf2,10000,salt,derived]", function() {
+        var testDeserialize = mcf.deserialize('$pbkdf2$10000$salt$derived');
+        testDeserialize.must.include("pbkdf2", 10000, "salt", "derived");
+        testDeserialize.must.be.an.object();
     });
 
-    describe("#deserialize",function() {
-        it("deserialize(mcf_field) should return an object [pbkdf2,10000,salt,derived]",function() {
-            var testDeserialize = mcf.deserialize(mcf_field);
-            testDeserialize.must.include("pbkdf2",10000,"salt","derived");
-            testDeserialize.must.be.an.object();
-        });
+    it("NaN must return an error", function() {
+        mcf.deserialize.bind('$pbkdf2$a$salt$derived').must.throw();
     });
 
-    describe("#serialize cost is null",function() {
-        it("deserialize(mcf_cost_null) should return an error", function() {
-            mcf.deserialize.bind(mcf_cost_null).must.throw();
-        });
+    it("Negative must return an error", function() {
+        mcf.deserialize.bind('$pbkdf2$-1$salt$derived').must.throw();
     });
 
-    describe("#serialize error regex",function() {
-        it("deserialize(mcf_cost_error) should return an error of regex", function() {
-            mcf.deserialize.bind(mcf_cost_error).must.throw();
-        });
+    it("0 must return an error", function() {
+        mcf.deserialize.bind('$pbkdf2$0$salt$derived').must.throw();
+    });
+
+    it("deserialize(mcf_cost_error) should return an error of regex", function() {
+        mcf.deserialize.bind('$-15pbkdf2$0$salt$derived').must.throw();
     });
 
 });
