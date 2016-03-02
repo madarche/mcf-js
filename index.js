1682d75db2b63b0014a128613abb14ea3122c346
Marc-Aurèle DARCHE <ma.darche@cynode.org>
2016-03-02 13:00:08 +0100

    Remove and disallow useless semicolons (semi)

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
-const createError = require('create-error');
+const createError = require('create-error')
 
-const McfError = createError('McfError');
+const McfError = createError('McfError')
 
 // http://pythonhosted.org/passlib/modular_crypt_format.html
 // salt must be a base64 encoded string
 // derived_key must be a base64 encoded string
 // '$'+identifier+'$'+cost+'$'+salt+'$'+derived_key
-const MCF_REGEXP = /\$(\w+)\$(\d+)\$([A-Za-z0-9_-]+)\$([A-Za-z0-9_-]+)/;
+const MCF_REGEXP = /\$(\w+)\$(\d+)\$([A-Za-z0-9_-]+)\$([A-Za-z0-9_-]+)/
 
 /**
  * @param {string} mcf_field
  * @returns {Object}
  */
 function deserialize(mcf_field) {
-    let match = MCF_REGEXP.exec(mcf_field);
+    let match = MCF_REGEXP.exec(mcf_field)
     if (!match) {
         throw new McfError("Invalid MCF: " + mcf_field +
-                           " while it must be: " + MCF_REGEXP);
+                           " while it must be: " + MCF_REGEXP)
     }
-    let identifier = match[1];
+    let identifier = match[1]
 
-    let cost = Number(match[2]);
+    let cost = Number(match[2])
     if (cost === 0) {
         throw new McfError("Invalid MCF: " + cost +
-                           " must be a strictly positive number");
+                           " must be a strictly positive number")
     }
 
-    let salt = match[3];
+    let salt = match[3]
 
-    let derived_key = match[4];
+    let derived_key = match[4]
 
     return {
         identifier: identifier,
         cost: cost,
         salt: salt,
         derived_key: derived_key
-    };
+    }
 }
 
 /**
@@ -49,9 +49,9 @@ function deserialize(mcf_field) {
  */
 function serialize(identifier, cost, salt, derived_key) {
     return '$' + identifier + '$' + cost +
-        '$' + salt + '$' + derived_key ;
+        '$' + salt + '$' + derived_key
 }
 
-exports.McfError = McfError;
-exports.serialize = serialize;
-exports.deserialize = deserialize;
+exports.McfError = McfError
+exports.serialize = serialize
+exports.deserialize = deserialize
--- test/lib.test.js
+++ test/lib.test.js
@@ -1,48 +1,48 @@
-'use strict';
+'use strict'
 
-const expect = require('must');
+const expect = require('must')
 
-const mcf = require('../');
-const McfError = mcf.McfError;
+const mcf = require('../')
+const McfError = mcf.McfError
 
 describe("MCF", function() {
 
     it("must export McfError", function() {
-        expect(McfError).to.exist();
-    });
+        expect(McfError).to.exist()
+    })
 
     it("serialize must produce a string", function() {
-        let res = mcf.serialize('pbkdf2', 10000, 'some_salt', 'some_derived_key');
-        res.must.be.a.string();
-        res.must.equal('$pbkdf2$10000$some_salt$some_derived_key');
-    });
+        let res = mcf.serialize('pbkdf2', 10000, 'some_salt', 'some_derived_key')
+        res.must.be.a.string()
+        res.must.equal('$pbkdf2$10000$some_salt$some_derived_key')
+    })
 
     it("deserialize must accept only a valid field", function() {
         // TODO: Change to use this when js-must is fixed to accept this
         //mcf.deserialize('an_invalid_field').must.throw();
         try {
-            mcf.deserialize('an_invalid_field').must.throw();
+            mcf.deserialize('an_invalid_field').must.throw()
         } catch (err) {
-            expect(err).to.exist();
-            err.must.be.a(McfError);
+            expect(err).to.exist()
+            err.must.be.a(McfError)
         }
-    });
+    })
 
     it("deserialize must accept only a strictly positive (not zero) cost", function() {
         // TODO: Change to use this when js-must is fixed to accept this
         //mcf.deserialize('$pbkdf2$0$some_salt$some_derived_key').must.throw();
         try {
-            mcf.deserialize('$pbkdf2$0$some_salt$some_derived_key').must.throw();
+            mcf.deserialize('$pbkdf2$0$some_salt$some_derived_key').must.throw()
         } catch (err) {
-            expect(err).to.exist();
-            err.must.be.a(McfError);
+            expect(err).to.exist()
+            err.must.be.a(McfError)
         }
-    });
+    })
 
     it("deserialize must return an object", function() {
-        let res = mcf.deserialize('$pbkdf2$10000$some_salt$some_derived_key');
-        res.must.be.an.object();
-        res.must.include('pbkdf2', 10000, 'some_salt', 'some_derived_key');
-    });
+        let res = mcf.deserialize('$pbkdf2$10000$some_salt$some_derived_key')
+        res.must.be.an.object()
+        res.must.include('pbkdf2', 10000, 'some_salt', 'some_derived_key')
+    })
 
-});
+})
